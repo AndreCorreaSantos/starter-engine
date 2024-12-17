79306490f15175e458ac40840408e7f3829e92ce
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-12-17 16:30:17 -0300

    checkpoint 3

--- engineLang/include/EngineOps.h
+++ engineLang/include/EngineOps.h
@@ -1,16 +1,15 @@
 #ifndef ENGINE_OPS_H
 #define ENGINE_OPS_H
 
-// Include necessary MLIR headers
+#include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/OpDefinition.h"
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Builders.h"
 
-// Forward declaration of the EngineDialect
-#include "EngineDialect.h"
+namespace mlir {
+namespace engine {
 
-// Include TableGen-generated operation declarations
-#define GET_OP_DECLS
 #include "EngineOps.h.inc"
+
+} // namespace engine
+} // namespace mlir
 
 #endif // ENGINE_OPS_H
