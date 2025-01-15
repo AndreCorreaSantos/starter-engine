1c302249c897335f3403a62240910116191bc736
AndreCorreaSantos <acorreasantos2002@gmail.com>
2025-01-14 23:43:53 -0300

    calyx to fsm not yet working

--- engineDialect/circt_lowering/circt_run.sh
+++ engineDialect/circt_lowering/circt_run.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# Clear the log file at the beginning
+
+> log.mlir
+
 out="../build/bin/engine-opt --mlir-print-ir-after-all input.mlir"
 
-eval "$out" | circt-opt \
-  --pass-pipeline="builtin.module(
+enginePass=$(eval "$out")
+
+echo "$enginePass" >> log.mlir
+
+echo "$enginePass" | circt-opt --mlir-print-ir-after-all \
+ --pass-pipeline="builtin.module(
     func.func(convert-affine-to-loopschedule),
-    lower-scf-to-calyx)" > output.mlir
+    lower-scf-to-calyx,
+    calyx.component(lower-calyx-to-fsm)
+  )" > output.mlir 2>> log.mlir
+
+
+#calyx.component(lower-calyx-to-fsm) calyx to fsm
+
+# loopschedule to calyx
+# calyx to fsm
+# fsm to sv
+# export verilog
--- engineDialect/circt_lowering/input.mlir
+++ engineDialect/circt_lowering/input.mlir
@@ -1,8 +1,11 @@
 module {
   func.func @main() {
+
+  "engine.settings"() { value = 1 : i32 } : () -> ()
   %0 = "engine.constant"() {value = dense<[1.000000e+00, 2.000000e+00, 3.000000e+00]> : tensor<3xf64>} : () -> memref<3xf64>
   %1 = "engine.constant"() {value = dense<[1.000000e+00, 2.000000e+00, 3.000000e+00]> : tensor<3xf64>} : () -> memref<3xf64>
   %2 = "engine.add"(%0, %1) : (memref<3xf64>, memref<3xf64>) -> memref<3xf64>
+  
   func.return
   }
 }
